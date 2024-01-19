7743546316dfc924d3ea17f781ba0c63b14df4a0
Mohammed Samy <118897322+Eanzo22@users.noreply.github.com>
2024-01-19 23:24:51 +0200

    final

--- script.js
+++ script.js
@@ -49,30 +49,35 @@ function shuffle(array) {
 }
 //Change Answers & Questions content
 function ChangeContent(array, Current_Question, Html_Element) {
+//clear the choices content 
   $(Html_Element).empty();
-  // console.log(QuestionsArr[Current_Question].QuestionText);
+  //change the Question text
   $(".question span").text(QuestionsArr[Current_Question].QuestionText);
+  //check the current index to hide or show the next and previous Buttons  
   checkIndex(Current_Question);
+  //display the answers text of that question
   array[Current_Question].Answers.forEach((AnswerText) => {
     var label = $(`<label for="${AnswerText}">${AnswerText}</label>`);
-    var radio = $(
-      `<input type="radio" name="choices" id="${AnswerText}" value="${AnswerText}">`
-    );
+    var radio = $(`<input type="radio" name="choices" id="${AnswerText}" value="${AnswerText}">`);
     var answersDiv = $("<div></div>").append(label).append(radio);
     $(".answers").append(answersDiv);
     answersDiv.fadeIn();
+    //update the page index 
     $(".question-number").text(Current_Question + 1);
   });
   $("input[name='choices']").on("change", () => {
+    //save the answered choices
     var value = $("input[name='choices']:checked").val();
     var QuestionId = QuestionsArr[Current_Question].QuestionID;
     AnswersArr[Current_Question] = { id: QuestionId, answer: value };
-      var progressBar = $(".progress");
+    //update the progress bar
+    var progressBar = $(".progress");
       progressBar[0].style.setProperty("--webkit-progress-value-color",getColor(progressBarValue));
       progressBar.val(progressBarValue);
       progressBarValue += progressBarStep;
   });
 }
+//well .. the name is enough 
 function searchQuestionById(id) {
   return QuestionsArr.find((question) => question.QuestionID == id);
 }
@@ -82,6 +87,7 @@ function CheckRadios(array, CurrentPosition) {
   // console.log(array[CurrentPosition].answer)
   $(`#${array[CurrentPosition].answer}`).prop("checked", true);
 }
+//add marked questions to the marked list
 function AddMarkedQuestion(QID, QArray, QIndex, AnsArray) {
   //add the div
   if ($(`#marked-question${QID}`).length == 0) {
@@ -92,6 +98,7 @@ function AddMarkedQuestion(QID, QArray, QIndex, AnsArray) {
     );
     $(".marked-questions").append(MarkedDiv);
     $(".marked-questions-content").on("click", function () {
+      // debugger
       CurrentQuestion = QIndex;
       console.log(CurrentQuestion);
       ChangeContent(QArray, QIndex, ".answers");
@@ -178,33 +185,38 @@ function getColor(value) {
 var QuestionsArr = [
   new Question(
     1,
-    "this is a test question1",
-    ["answer1", "answer2", "answer3", "answer4", "answer5"],
-    "answer1"
+    "What does the abbreviation HTML stand for?",
+    [
+      "HyperText Markup Language",
+      "HighText Markup Language",
+      "HyperText Markdown Language",
+      "None of the above",
+    ],
+    "HyperText Markup Language"
   ),
   new Question(
     2,
-    "this is a test question2",
-    ["answer1", "answer2", "answer3", "answer4", "answer5"],
-    "answer2"
+    "Which of the following CSS framework is used to create a responsive design?",
+    ["bootstrap", "laravel", "rails", "django"],
+    "bootstrap"
   ),
   new Question(
     3,
-    "this is a test question3",
-    ["answer1", "answer2", "answer3", "answer4", "answer5"],
-    "answer3"
+    "Which of the following CSS selectors are used to specify a group of elements?",
+    ["tag", "id", "class", "both class and tag"],
+    "class"
   ),
   new Question(
     4,
-    "this is a test question4",
-    ["answer1", "answer2", "answer3", "answer4", "answer5"],
-    "answer4"
+    "Which of the following has introduced text, list, box, margin, border, color, and background properties?",
+    ["Html", "PHP", "CSS", "Ajax"],
+    "CSS"
   ),
   new Question(
     5,
-    "this is a test question5",
-    ["answer1", "answer2", "answer3", "answer4", "answer5"],
-    "answer5"
+    "What's the name of our coding filler in JS ",
+    ["Hamada", "Hamoda", "Hamo", "Dodo"],
+    "Hamada"
   ),
 ];
 var AnswersArr = [];
@@ -260,25 +272,25 @@ $(document).ready(() => {
   const timerInterval = setInterval(updateTimer, 1000);
   //===================================================
   //Setting timeout
-//   setTimeout(() => {
-//     CurrentQuestion = 0;
-//     AnswersArr.forEach((Answer) => {
-//       if (Answer.answer == QuestionsArr[CurrentQuestion].RightAnswer) {
-//         submissionDetails.numberOfRightAnswers++;
-//         submissionDetails.rightAnswers.push(searchQuestionById(Answer.id));
-//       } else
-//         submissionDetails.wrongAnswers.push({
-//           question: searchQuestionById(Answer.id),
-//           userAnswer: Answer.answer,
-//         });
-//       CurrentQuestion++;
-//     });
-//     console.log(submissionDetails);
-//     CurrentQuestion = $(".question-number").text() - 1;
-//     let jsonString = JSON.stringify(submissionDetails);
-//     localStorage.setItem("examData", jsonString);
-//     location.replace("test.html");
-//   }, timeInSeconds * 1000);
+  setTimeout(() => {
+    CurrentQuestion = 0;
+    AnswersArr.forEach((Answer) => {
+      if (Answer.answer == QuestionsArr[CurrentQuestion].RightAnswer) {
+        submissionDetails.numberOfRightAnswers++;
+        submissionDetails.rightAnswers.push(searchQuestionById(Answer.id));
+      } else
+        submissionDetails.wrongAnswers.push({
+          question: searchQuestionById(Answer.id),
+          userAnswer: Answer.answer,
+        });
+      CurrentQuestion++;
+    });
+    console.log(submissionDetails);
+    CurrentQuestion = $(".question-number").text() - 1;
+    let jsonString = JSON.stringify(submissionDetails);
+    localStorage.setItem("examData", jsonString);
+    location.replace("test.html");
+  }, timeInSeconds * 1000);
   //============================================================================================
   //Next Button functionality
   $("#next").on("click", () => {
